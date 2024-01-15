786c79ac04e425b1dc41ef44c156e93680f61108
UBC Student <kennyjiaxi@hotmail.com>
2024-01-15 13:53:16 +0100

    Added Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './bootstrap.min.css';
+import NavBar from "./Components/NavBar";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
+        <Container>
+
+        </Container>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
